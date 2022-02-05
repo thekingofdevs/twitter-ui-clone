b79457ea660193b08ad05cd73c5590aceb9dc4e7
Otrebor <otreborsama>
2022-02-04 23:51:48 -0400

    v1.1

---
+++ js/author.js
@@ -0,0 +1,10 @@
+(function () {
+  const author = {
+    autor: "The King of Devs",
+    mensaje:
+      "Sígueme para que no te pierdas de mi contenido, espero y disfrutes el proyecto.",
+    redes: "@thekingofdevs",
+  };
+
+  return console.log(author);
+})();
